c5d28b60d7ba320a28c9031d96b4730fc9b50a27
alibekshak <alibekshakiro1@gmail.com>
2024-04-23 10:04:08 +0500

    Improve code reading in GenerSelectionView

--- GPT/auxiliary/GenreView/GenerSelectionView.swift
+++ GPT/auxiliary/GenreView/GenerSelectionView.swift
@@ -26,85 +26,11 @@ struct GenreSelectionView: View {
             Color(red: 240/255, green: 240/255, blue: 240/255)
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button {
-                        withAnimation {
-                            dismiss()
-                        }
-                    } label: {
-                        Image(systemName: "xmark")
-                            .font(Font.system(size: 24, weight: .semibold))
-                            .foregroundColor(.black)
-                    }
-                }
-                .padding(.trailing, 30)
-                .padding(.bottom)
-                
-                Text("Выбор жанров повлияет на ход диалога о книгах или авторе")
-                    .font(.headline)
-                    .foregroundColor(.black)
-                    .padding(.horizontal, 40)
-                
-                Text("Выберите жанры:")
-                    .font(.headline)
-                    .padding(.top, 20)
-                    .foregroundColor(.black)
-                
-                ScrollView {
-                    ForEach(BookGenre.allCases, id: \.self) { genre in
-                        Button(action: {
-                            if temporarySelectedGenres.contains(genre) {
-                                temporarySelectedGenres.remove(genre)
-                            } else {
-                                temporarySelectedGenres.insert(genre)
-                            }
-                        }) {
-                            HStack {
-                                Text(genre.rawValue)
-                                    .foregroundColor(.black)
-                                if temporarySelectedGenres.contains(genre) {
-                                    Spacer()
-                                    Image(systemName: "checkmark.square.fill")
-                                        .foregroundColor(.blue)
-                                } else {
-                                    Spacer()
-                                    Image(systemName: "square")
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            .padding(.horizontal, 80)
-                            .padding(.vertical, 4)
-                        }
-                    }
-                }.padding(.vertical, 35)
-                
-                Spacer()
-                
-                
-                Button(action: {
-                    selectedGenres = temporarySelectedGenres
-                    isGenreSelectionCompleted = true
-                    appState.isGenreSelectionCompleted = true
-                    
-                    let genresData = try? JSONEncoder().encode(selectedGenres)
-                    UserDefaults.standard.set(genresData, forKey: "selectedGenres")
-                    withAnimation {
-                        dismiss()
-                    }
-                }) {
-                    Text("Далее")
-                        .frame(width: 307, height: 44)
-                        .foregroundColor(Color.white)
-                        .font(.system(size: 22))
-                        .background(Color(red: 30/255, green: 30/255, blue: 30/255))
-                        .cornerRadius(10)
-                        .padding()
-                }
-                .disabled(temporarySelectedGenres.isEmpty) // Disable the button if no genres are selected
+                dismissButton
+                textInfo
+                scrollView
+                buttonNext
             }
-            .navigationBarTitle("", displayMode: .inline)
             .navigationBarHidden(true)
             .onAppear {
                 temporarySelectedGenres = selectedGenres
@@ -119,6 +45,90 @@ struct GenreSelectionView: View {
                 UserDefaults.standard.set(genresData, forKey: "selectedGenres")
             }
         }
+    }
+    
+    var dismissButton: some View {
+        HStack {
+            Spacer()
+            Button {
+                withAnimation {
+                    dismiss()
+                }
+            } label: {
+                Image(systemName: "xmark")
+                    .font(Font.system(size: 24, weight: .semibold))
+                    .foregroundColor(.black)
+            }
+            .disabled(temporarySelectedGenres.isEmpty)
+        }
+        .padding(.trailing, 30)
+        .padding(.bottom)
+    }
+    
+    var textInfo: some View {
+        VStack {
+            Text("Выбор жанров повлияет на ход диалога о книгах или авторе")
+                .padding(.horizontal, 40)
+            Text("Выберите жанры:")
+                .padding(.top, 20)
+        }
+        .font(.headline)
+        .foregroundColor(.black)
+
+    }
+    
+    var scrollView: some View {
+        ScrollView {
+            ForEach(BookGenre.allCases, id: \.self) { genre in
+                Button(action: {
+                    if temporarySelectedGenres.contains(genre) {
+                        temporarySelectedGenres.remove(genre)
+                    } else {
+                        temporarySelectedGenres.insert(genre)
+                    }
+                }) {
+                    HStack {
+                        Text(genre.rawValue)
+                            .foregroundColor(.black)
+                        if temporarySelectedGenres.contains(genre) {
+                            Spacer()
+                            Image(systemName: "checkmark.square.fill")
+                                .foregroundColor(.blue)
+                        } else {
+                            Spacer()
+                            Image(systemName: "square")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.horizontal, 80)
+                    .padding(.vertical, 4)
+                }
+            }
+        }
+        .padding(.vertical, 35)
+    }
+    
+    var buttonNext: some View {
+        Button(action: {
+            selectedGenres = temporarySelectedGenres
+            isGenreSelectionCompleted = true
+            appState.isGenreSelectionCompleted = true
+            
+            let genresData = try? JSONEncoder().encode(selectedGenres)
+            UserDefaults.standard.set(genresData, forKey: "selectedGenres")
+            withAnimation {
+                dismiss()
+            }
+        }) {
+            Text("Далее")
+                .frame(width: 307, height: 44)
+                .foregroundColor(Color.white)
+                .font(.system(size: 22))
+                .background(Color(red: 30/255, green: 30/255, blue: 30/255))
+                .cornerRadius(10)
+                .padding()
+        }
+        .disabled(temporarySelectedGenres.isEmpty)
     }
 }
 
