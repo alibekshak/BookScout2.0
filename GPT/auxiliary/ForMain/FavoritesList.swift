c3ded219494608bdedc4c55fc276511bdbe6fa95
alibekshak <alibekshakiro1@gmail.com>
2024-05-02 08:49:30 +0500

    UI and code changes at FavoritesList

--- GPT/auxiliary/ForMain/FavoritesList.swift
+++ GPT/auxiliary/ForMain/FavoritesList.swift
@@ -10,32 +10,41 @@ struct FavoritesListView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(viewModel.favoriteItems) { item in
-                    Text(item.title)
+            VStack {
+                navigationBar
+                List {
+                    ForEach(viewModel.favoriteItems) { item in
+                        Text(item.title)
+                    }
+                    .onDelete(perform: viewModel.deleteFavoriteItem)
+                    .onMove(perform: viewModel.moveFavoriteItem)
                 }
-                .onDelete(perform: viewModel.deleteFavoriteItem)
-                .onMove(perform: viewModel.moveFavoriteItem)
+                .environment(\.editMode, isEditing ? .constant(.active) : .constant(.inactive))
             }
-            .navigationBarTitle("Избранное")
-            .navigationBarItems(trailing:
-                                    HStack(spacing: 240) {
-                Button(action: {
-                    isEditing.toggle()
-                }, label: {
-                    Image(systemName: isEditing ? "arrow.up.arrow.down" : "line.horizontal.3")
-                        .font(.title2)
-                })
-                Button("Закрыть") {
-                    dismiss()
-                }
-                .offset(x: -15)
-            })
-            .environment(\.editMode, isEditing ? .constant(.active) : .constant(.inactive))
         }
         .onAppear {
             viewModel.refreshFavoriteItems()
         }
+    }
+    
+    var navigationBar: some View {
+        HStack(spacing: 50) {
+            Button(action: {
+                isEditing.toggle()
+            }, label: {
+                Image(systemName: isEditing ? "line.3.horizontal.decrease" : "line.horizontal.3")
+                    .font(.title2)
+            })
+            Text("Избранное")
+                .font(.title)
+                .foregroundColor(.black)
+                .padding(.leading)
+            Button("Закрыть") {
+                dismiss()
+            }
+        }
+        .padding(.top)
+        .padding(.horizontal)
     }
 }
 
