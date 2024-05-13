4ec8de05b882ebe161fcca8b2eab22f72e26527a
alibekshak <alibekshakiro1@gmail.com>
2024-05-13 09:47:47 +0500

    Delete unused code

--- GPT/ViewModel/ChatViewModel.swift
+++ GPT/ViewModel/ChatViewModel.swift
@@ -1,6 +1,5 @@
 import Foundation
 import SwiftUI
-import AVKit
 
 class ChatViewModel: ObservableObject {
     
@@ -8,15 +7,10 @@ class ChatViewModel: ObservableObject {
     @Published var messages: [MessageRow] = []
     @Published var inputMessage: String = ""
     
-    private var synthesizer: AVSpeechSynthesizer?
-    
     private let api: ChatGPTAPI
     
     init(api: ChatGPTAPI, enableSpeech: Bool = false) {
         self.api = api
-        if enableSpeech {
-            synthesizer = .init()
-        }
         Task {
             if let selectedGenresData = UserDefaults.standard.data(forKey: "selectedGenres") {
                 if let selectedGenres = try? JSONDecoder().decode(Set<BookGenre>.self, from: selectedGenresData) {
@@ -38,7 +32,6 @@ class ChatViewModel: ObservableObject {
     
     @MainActor
     func clearMessages() {
-        stopSpeaking()
         api.deleteHistoryList()
         withAnimation { [weak self] in
             self?.messages = []
@@ -82,25 +75,6 @@ class ChatViewModel: ObservableObject {
         messageRow.isInteractingWithChatGPT = false
         self.messages[self.messages.count - 1] = messageRow
         isInteractingWithChatGPT = false
-        speakLastResponse()
-        
-    }
-    
-    func speakLastResponse() {
-        guard let synthesizer, let responseText = self.messages.last?.responseText, !responseText.isEmpty else {
-            return
-        }
-        stopSpeaking()
-        let utterance = AVSpeechUtterance(string: responseText)
-        utterance.voice = .init(language: "en-US")
-        utterance.rate = 0.5
-        utterance.pitchMultiplier = 0.8
-        utterance.postUtteranceDelay = 0.2
-        synthesizer.speak(utterance )
-    }
-    
-    func stopSpeaking() {
-        synthesizer?.stopSpeaking(at: .immediate)
     }
     
     @MainActor func refreshChat() {
