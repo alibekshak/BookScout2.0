1c4f2cc74c6a64dfb2025de435a701479272353c
alibekshak <alibekshakiro1@gmail.com>
2024-05-14 09:05:18 +0500

    New LaunchScreen

--- GPT/View/LaunchScreen/LaunchScreen.swift
+++ GPT/View/LaunchScreen/LaunchScreen.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct LaunchScreen: View {
+    
+    @State private var isAnimating = false
+    
     var body: some View {
         ZStack {
             CustomColors.backgroundColor
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
+            VStack(spacing: 20) {
                 Text("BookScout")
-                    .font(.system(size: 36, weight: .bold))
+                    .font(.system(size: 46, weight: .bold))
                     .foregroundColor(Color.black)
                 Image(systemName: "book.closed")
                     .font(
@@ -25,10 +28,14 @@ struct LaunchScreen: View {
                         )
                     )
                     .rotationEffect(.degrees(15), anchor: .center)
-                ProgressView()
-                   
             }
-
+            .scaleEffect(isAnimating ? 1 : 0.5)
+            .animation(.easeInOut(duration: 1.3), value: isAnimating)
+            ProgressView()
+                .padding(.top, 280)
+        }
+        .onAppear {
+            self.isAnimating = true
         }
     }
 }
